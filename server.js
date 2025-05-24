de0de513f159a6ee2c823294af92c609126314ac
Nylon1 <71017473+Nylon1@users.noreply.github.com>
2025-05-24 14:40:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -82,6 +82,3 @@ mongoose.connect(process.env.MONGO_URI, {
   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 }).catch(err => console.error('❌ MongoDB error:', err));
 
-
-
-
