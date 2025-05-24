04997fab5524b15b31b2c18a2ab45e7a7c3c24a6
Nylon1 <71017473+Nylon1@users.noreply.github.com>
2025-05-24 20:13:21 +0100

    routes

--- routes/public.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-
-// ✅ Public route to get approved content
-router.get('/approved', adminController.getApprovedContent);
-
-module.exports = router;
