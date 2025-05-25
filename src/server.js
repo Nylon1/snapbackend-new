288cb99b6cedb8af859f8d8d587a172fdadfbed8
Nylon1 <71017473+Nylon1@users.noreply.github.com>
2025-05-25 15:00:55 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -60,7 +60,7 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 24
   }
 }));
- else in-memory store will drop on every restart:
+ 
   store: new (require('connect-mongo')(session))({
     mongooseConnection: mongoose.connection
   }),
